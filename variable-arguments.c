98b488d792cfaa36a09b7e63a2a6b3b44ec4737d
brooknone <brook.none@qq.com>
2019-12-27 18:36:18 +0800

    Date and time
    Resolve #3
    
    Signed-off-by: brooknone <brook.none@qq.com>

--- variable-arguments.c
+++ variable-arguments.c
@@ -41,7 +41,7 @@ void func(int num, ...)
 
     for (int i = 0; i < num; i++) {
         ch = va_arg(valist2, char*);
-        if(ch == "") break;
+        if(strcmp(ch, "") == 0) break;
         printf("%d: %s", i, ch);
     }
     printf("\n");
@@ -49,10 +49,8 @@ void func(int num, ...)
     va_end(valist2);
 }
 
-
-
 int main() {
-    printf("Product of sum and n: %d\n", pro_sum(2, 5,10,20));
-    printf("Average of sum1: %f\n", average(4, 20,10,15,35));
-    func(4, "Dark ","Nick ","Brook ","Bonnie ");
+    printf("Product of sum and n: %d\n", pro_sum(2, 5, 10, 20));
+    printf("Average of sum1: %f\n", average(4, 20, 10, 15, 35));
+    func(4, "Dark ", "Nick ", "Brook ", "Bonnie ");
 }
